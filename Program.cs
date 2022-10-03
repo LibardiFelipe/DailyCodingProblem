f610c02f1448e98854165cce5813a3ec4e670da2
Felipe Libardi <flibardig@gmail.com>
2022-10-02 21:34:09 -0300

    Enunciados traduzidos para português.

--- Program.cs
+++ Program.cs
@@ -5,17 +5,19 @@
         static void Main(string[] args)
         {
             /*
-             * Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
-             * For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
+             * Este desafio foi proposto recentemente pela Google.
+             * Dado uma lista de números e um número (k), retorna true caso dois números dessa lista somados seja igual à k.
+             * Por exemplo, dado [10, 15, 3, 7] e k = 17, retorna true, pois 10 + 7 é 17.
              */
             var list = new List<int> { 10, 15, 3, 7 };
             Console.WriteLine(Google.ProblemOne(list, 17));
 
             /*
-             * Given an array of integers, return a new array such that each element at
-             * index i of the new array is the product of all the numbers in the original array except the one at i.
-             * For example, if our input was [1, 2, 3, 4, 5], the expected output would be [120, 60, 40, 30, 24].
-             * If our input was [3, 2, 1], the expected output would be [2, 3, 6].
+             * Este desafio foi proposto pela Uber.
+             * Dado um array de inteiros, retorna um novo array em que cada elemento
+             * (i) do novo array é o produto de todos os números do array original, exceto o do i.
+             * Por exemplo, se o input for [1, 2, 3, 4, 5], o output esperado será [120, 60, 40, 30, 24].
+             * Se o input for [3, 2, 1], o output esperado será [2, 3, 6].
              */
             int[] nums = { 1, 2, 3, 4, 5 };
             Console.WriteLine("{ " + $"{string.Join(", ", Uber.ProblemOne(nums))}" + " }");
