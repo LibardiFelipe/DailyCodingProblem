3d2655308a43f43a4126fdbaf91d18f59934e227
Felipe Libardi <flibardig@gmail.com>
2022-10-04 18:47:45 -0300

    Adicionado classe para exibir mensagens com cores diferentes.

---
+++ Print.cs
@@ -0,0 +1,13 @@
+﻿namespace DailyCodingProblem.Companies
+{
+    public static class Print
+    {
+        public static void ChallengeInfos(string title, string description)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(title);
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine(description);
+        }
+    }
+}
